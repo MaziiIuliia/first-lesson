b03c310b868959e1d71fedce5f8f0debfc13f4c8
Mazii Iuliia <mazii.iuliia@gmail.com>
2016-09-14 16:46:37 +0300

    resolve issues and add other tasks

--- maindataclasses.rb
+++ maindataclasses.rb
@@ -1,22 +1,33 @@
 # task 1
+puts "task1"
 string1 = '235'
-sum = string1[0].to_i+string1[1].to_i+string1[2].to_i
+v = 1
+len = string1.length
+sum = string1[0].to_i
+while v < len
+  sum += string1[v].to_i
+  v += 1
+end
 puts sum
 
 # task 2
+puts "task2"
 string2 = 'afsdf3432df23dj334'
 array = string2.scan(/[0-999]+/)
 puts array.max_by{ |elem| elem.size }.length
 
+
 # task 3
+puts "task3"
 string2 = 'afsdf3432df23dj334'
 array = string2.scan(/[0-999]+/)
 puts array.max_by{ |elem| elem.size }
 
+
 # task 4
+puts "task4"
 string3 = '1234gdgd'
 string4 = '1234asasas'
-string5 = '1234gdgd'
 array2 = []
 n = 0
 while string3[n] == string4[n]
@@ -26,24 +37,26 @@ end
 puts array2.length
 
 # task 5
+puts "task5"
 array3 = [1,2,3,4,5,6,7,8,9]
 array4 = []
 array5 = []
 a = 1
 idx = array3.index(array3[-1])
 while a <= idx
-    if array3.index(array3[a]).odd?
-      array4 << array3[a]
-    elsif array3.index(array3[a]).even?
-      array5 << array3[a]
-    end
-    a += 1
+  if array3.index(array3[a]).odd?
+    array4 << array3[a]
+  elsif array3.index(array3[a]).even?
+    array5 << array3[a]
+  end
+  a += 1
 end
 puts array4
 puts array5
 
 
 # task 6
+puts "task6"
 ary = [1,2,3,4,5,6,7,11,9]
 ary2 = []
 i = ary.index(ary[-2])
@@ -56,19 +69,37 @@ while ary2.empty? && i > 0
 end
 
 # task 7
+puts "task7"
 ary3 = [1,2,3,4,5,6,7,8]
 z = 1
 l = ary3.length
-while z > 0 && z < (l-1)
+while z < (l-1)
   if ary3[z].even?
-   ary3[z] += ary3[0]
+    ary3[z] += ary3[0]
   end
   z += 1
 end
 puts ary3
 
 # task 8
-hash = {'key1':'value1', 'key2':'value2'}
-hash.each_pair do |key,value|
-  puts hash.keys
+puts "task8"
+hash = {'keyone' => 'value', 'keytwo' => 'value'}
+hash.each_key {|key| key.to_i}
+hash.each_value {|value| value.to_sym}
+puts hash
+
+# task 9
+puts "task9"
+hash2 = {symbol: 1, asymbol: 2, symb: 1, mbol: 2,}
+ary4 = hash2.keys
+ary4.map!{|el|el.to_s}
+g = 0
+h = ary4.length
+while g < h
+  str = ary4[g]
+  if str[0] == 's'
+    hash2.delete_if {|key, value| key == str.to_sym }
+  end
+  g += 1
 end
+puts hash2
